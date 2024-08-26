7ee97449a56bede72ed22185a94eef3dd69237b1
Owais Nadeem <108211200+owaisnadeem18@users.noreply.github.com>
2024-08-27 02:10:38 +0500

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,97 @@
 import React from "react";
 import { useState } from "react";
 import "./App.css";
+import { v4 as uuidv4 } from "uuid";
 
 export default function App() {
-  const [todo, SetTodo] = useState(["By Default"]);
+  const [todo, SetTodo] = useState("By Default");
 
-  const [todos, Settodos] = useState(["EAT", "SLEEP", "REPEAT"]);
+  const [todos, Settodos] = useState([{ task: "Default Task", id: uuidv4() }]);
 
   const AddTask = () => {
-    Settodos([...todos, todo]);
+    Settodos((todos) => {
+      return [...todos, { task: todo, id: uuidv4(), isDone: false }];
+    });
+
+    SetTodo("");
   };
 
   // Function to edit a todo task
 
-  function EditFunc() {
-    console.log("Edit Function Called");
+  function EditFunc(id) {
+    Settodos(
+      todos.filter((tasks) => {
+        return tasks.id != id;
+      })
+    );
+
+    const taskToEdit = todos.find((task) => task.id == id);
+
+    SetTodo(taskToEdit.task);
   }
 
   // Function to delete a todo task
 
-  function DeleteFunc() {
-    console.log("Delete Function Called");
+  function DeleteFunc(id) {
+    Settodos((todosElem) => todosElem.filter((task) => task.id != id));
+  }
+
+  // Function to convert all into upper case
+
+  function UpperCaseALL() {
+    Settodos((elemts) =>
+      elemts.map((elem) => {
+        return {
+          ...elem,
+          task: elem.task.toUpperCase(),
+        };
+      })
+    );
+  }
+
+  // Function to convert single todo into upper case
+
+  function UpperCaseOne(id) {
+    Settodos((upperCaseOne) =>
+      upperCaseOne.map((SingleTodo) => {
+        if (SingleTodo.id == id) {
+          return {
+            ...SingleTodo,
+            task: SingleTodo.task.toUpperCase(),
+          };
+        } else {
+          return SingleTodo;
+        }
+      })
+    );
+  }
+
+  // Function to set mark as done
+
+  function FinishedTask(id) {
+    Settodos(
+      todos.map((elem) => {
+        if (elem.id == id) {
+          return {
+            ...elem,
+            isDone: true,
+          };
+        } else {
+          return elem;
+        }
+      })
+    );
+  }
+
+  function CompletedAll() {
+    Settodos((allTasks) =>
+      allTasks.map((task) => {
+        return {
+          ...task,
+          isDone: true,
+        };
+      })
+    );
   }
 
   return (
@@ -42,13 +113,41 @@ export default function App() {
 
       <div className="todos-container">
         <ul>
-          {todos.map((singleTask, ind) => (
-            <li key={ind}>
-              {singleTask} <button onClick={EditFunc}>Edit</button>{" "}
-              <button onClick={DeleteFunc}>Delete</button>{" "}
+          {todos.map((singleTask) => (
+            <li key={singleTask.id}>
+              <span
+                style={
+                  singleTask.isDone
+                    ? { textDecorationLine: "line-through" }
+                    : {}
+                }
+              >
+                {" "}
+                {singleTask.task}{" "}
+              </span>
+              &nbsp; &nbsp; &nbsp;
+              <button onClick={() => EditFunc(singleTask.id)}>Edit</button>{" "}
+              &nbsp; &nbsp; &nbsp;
+              <button onClick={() => DeleteFunc(singleTask.id)}>
+                Delete
+              </button>{" "}
+              &nbsp; &nbsp; &nbsp;
+              <button onClick={() => UpperCaseOne(singleTask.id)}>
+                Upper Case One
+              </button>
+              &nbsp; &nbsp; &nbsp;
+              <button onClick={() => FinishedTask(singleTask.id)}>
+                Done Task
+              </button>
+              <br />
+              <br />
             </li>
           ))}
         </ul>
+        <p>If you want to update all the tasks in the array then:</p>
+        <button onClick={UpperCaseALL}>TO UPPER CASE</button>
+        &nbsp; &nbsp;
+        <button onClick={CompletedAll}>All Tasks Completed</button>
       </div>
     </>
   );
